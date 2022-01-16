08e831124775b8ed500b8632a01cd4b6141b6f65
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2022-01-16 08:30:00 +0700

    Setup login with Username or Email

---
+++ resources/views/components/top-bar.blade.php
@@ -0,0 +1,273 @@
+<!-- BEGIN: Top Bar -->
+<div class="px-3 mb-12 -mx-3 border-b top-bar-boxed border-theme-2 -mt-7 md:-mt-5 sm:-mx-8 sm:px-8 md:pt-0">
+    <div class="flex items-center h-full">
+        <!-- BEGIN: Logo -->
+        <a href="" class="hidden -intro-x md:flex">
+            <img alt="Icewall Tailwind HTML Admin Template" class="w-6" src="/images/logo.svg">
+            <span class="ml-3 text-lg text-white"> Ice<span class="font-medium">wall</span> </span>
+        </a>
+        <!-- END: Logo -->
+        <!-- BEGIN: Breadcrumb -->
+        <div class="mr-auto -intro-x breadcrumb"> <a href="">Application</a> <i data-feather="chevron-right"
+                class="breadcrumb__icon"></i> <a href="" class="breadcrumb--active">Dashboard</a> </div>
+        <!-- END: Breadcrumb -->
+        <!-- BEGIN: Search -->
+        <div class="relative mr-3 intro-x sm:mr-6">
+            <div class="hidden search sm:block">
+                <input type="text"
+                    class="border-transparent search__input form-control dark:bg-dark-1 placeholder-theme-8"
+                    placeholder="Search...">
+                <i data-feather="search" class="search__icon dark:text-gray-300"></i>
+            </div>
+            <a class="notification sm:hidden" href=""> <i data-feather="search"
+                    class="notification__icon dark:text-gray-300"></i> </a>
+            <div class="search-result">
+                <div class="search-result__content">
+                    <div class="search-result__content__title">Pages</div>
+                    <div class="mb-5">
+                        <a href="" class="flex items-center">
+                            <div
+                                class="flex items-center justify-center w-8 h-8 rounded-full bg-theme-29 text-theme-10">
+                                <i class="w-4 h-4" data-feather="inbox"></i>
+                            </div>
+                            <div class="ml-3">Mail Settings</div>
+                        </a>
+                        <a href="" class="flex items-center mt-2">
+                            <div
+                                class="flex items-center justify-center w-8 h-8 rounded-full bg-theme-30 text-theme-24">
+                                <i class="w-4 h-4" data-feather="users"></i>
+                            </div>
+                            <div class="ml-3">Users & Permissions</div>
+                        </a>
+                        <a href="" class="flex items-center mt-2">
+                            <div
+                                class="flex items-center justify-center w-8 h-8 rounded-full bg-theme-31 text-theme-26">
+                                <i class="w-4 h-4" data-feather="credit-card"></i>
+                            </div>
+                            <div class="ml-3">Transactions Report</div>
+                        </a>
+                    </div>
+                    <div class="search-result__content__title">Users</div>
+                    <div class="mb-5">
+                        <a href="" class="flex items-center mt-2">
+                            <div class="w-8 h-8 image-fit">
+                                <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                    src="/images/profile-6.jpg">
+                            </div>
+                            <div class="ml-3">Denzel Washington</div>
+                            <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">
+                                denzelwashington@left4code.com</div>
+                        </a>
+                        <a href="" class="flex items-center mt-2">
+                            <div class="w-8 h-8 image-fit">
+                                <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                    src="/images/profile-5.jpg">
+                            </div>
+                            <div class="ml-3">Johnny Depp</div>
+                            <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">
+                                johnnydepp@left4code.com</div>
+                        </a>
+                        <a href="" class="flex items-center mt-2">
+                            <div class="w-8 h-8 image-fit">
+                                <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                    src="/images/profile-1.jpg">
+                            </div>
+                            <div class="ml-3">Brad Pitt</div>
+                            <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">
+                                bradpitt@left4code.com</div>
+                        </a>
+                        <a href="" class="flex items-center mt-2">
+                            <div class="w-8 h-8 image-fit">
+                                <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                    src="/images/profile-5.jpg">
+                            </div>
+                            <div class="ml-3">Johnny Depp</div>
+                            <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">
+                                johnnydepp@left4code.com</div>
+                        </a>
+                    </div>
+                    <div class="search-result__content__title">Products</div>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/preview-9.jpg">
+                        </div>
+                        <div class="ml-3">Samsung Galaxy S20 Ultra</div>
+                        <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">Smartphone &amp; Tablet
+                        </div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/preview-3.jpg">
+                        </div>
+                        <div class="ml-3">Nike Tanjun</div>
+                        <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">Sport &amp; Outdoor
+                        </div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/preview-1.jpg">
+                        </div>
+                        <div class="ml-3">Sony A7 III</div>
+                        <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">Photography</div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/preview-11.jpg">
+                        </div>
+                        <div class="ml-3">Samsung Galaxy S20 Ultra</div>
+                        <div class="w-48 ml-auto text-xs text-right text-gray-600 truncate">Smartphone &amp; Tablet
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- END: Search -->
+        <!-- BEGIN: Notifications -->
+        <div class="mr-4 intro-x dropdown sm:mr-6">
+            <div class="cursor-pointer dropdown-toggle notification notification--bullet" role="button"
+                aria-expanded="false"> <i data-feather="bell" class="notification__icon dark:text-gray-300"></i>
+            </div>
+            <div class="pt-2 notification-content dropdown-menu">
+                <div class="notification-content__box dropdown-menu__content box dark:bg-dark-6">
+                    <div class="notification-content__title">Notifications</div>
+                    <div class="relative flex items-center cursor-pointer ">
+                        <div class="flex-none w-12 h-12 mr-1 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/profile-6.jpg">
+                            <div
+                                class="absolute bottom-0 right-0 w-3 h-3 border-2 border-white rounded-full bg-theme-10">
+                            </div>
+                        </div>
+                        <div class="ml-2 overflow-hidden">
+                            <div class="flex items-center">
+                                <a href="javascript:;" class="mr-5 font-medium truncate">Denzel Washington</a>
+                                <div class="ml-auto text-xs text-gray-500 whitespace-nowrap">05:09 AM</div>
+                            </div>
+                            <div class="w-full truncate text-gray-600 mt-0.5">There are many variations of passages
+                                of Lorem Ipsum available, but the majority have suffered alteration in some form, by
+                                injected humour, or randomi</div>
+                        </div>
+                    </div>
+                    <div class="relative flex items-center mt-5 cursor-pointer">
+                        <div class="flex-none w-12 h-12 mr-1 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/profile-5.jpg">
+                            <div
+                                class="absolute bottom-0 right-0 w-3 h-3 border-2 border-white rounded-full bg-theme-10">
+                            </div>
+                        </div>
+                        <div class="ml-2 overflow-hidden">
+                            <div class="flex items-center">
+                                <a href="javascript:;" class="mr-5 font-medium truncate">Johnny Depp</a>
+                                <div class="ml-auto text-xs text-gray-500 whitespace-nowrap">01:10 PM</div>
+                            </div>
+                            <div class="w-full truncate text-gray-600 mt-0.5">It is a long established fact that a
+                                reader will be distracted by the readable content of a page when looking at its
+                                layout. The point of using Lorem </div>
+                        </div>
+                    </div>
+                    <div class="relative flex items-center mt-5 cursor-pointer">
+                        <div class="flex-none w-12 h-12 mr-1 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/profile-1.jpg">
+                            <div
+                                class="absolute bottom-0 right-0 w-3 h-3 border-2 border-white rounded-full bg-theme-10">
+                            </div>
+                        </div>
+                        <div class="ml-2 overflow-hidden">
+                            <div class="flex items-center">
+                                <a href="javascript:;" class="mr-5 font-medium truncate">Brad Pitt</a>
+                                <div class="ml-auto text-xs text-gray-500 whitespace-nowrap">05:09 AM</div>
+                            </div>
+                            <div class="w-full truncate text-gray-600 mt-0.5">Lorem Ipsum is simply dummy text of
+                                the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s
+                                standard dummy text ever since the 1500</div>
+                        </div>
+                    </div>
+                    <div class="relative flex items-center mt-5 cursor-pointer">
+                        <div class="flex-none w-12 h-12 mr-1 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/profile-5.jpg">
+                            <div
+                                class="absolute bottom-0 right-0 w-3 h-3 border-2 border-white rounded-full bg-theme-10">
+                            </div>
+                        </div>
+                        <div class="ml-2 overflow-hidden">
+                            <div class="flex items-center">
+                                <a href="javascript:;" class="mr-5 font-medium truncate">Johnny Depp</a>
+                                <div class="ml-auto text-xs text-gray-500 whitespace-nowrap">01:10 PM</div>
+                            </div>
+                            <div class="w-full truncate text-gray-600 mt-0.5">Contrary to popular belief, Lorem
+                                Ipsum is not simply random text. It has roots in a piece of classical Latin
+                                literature from 45 BC, making it over 20</div>
+                        </div>
+                    </div>
+                    <div class="relative flex items-center mt-5 cursor-pointer">
+                        <div class="flex-none w-12 h-12 mr-1 image-fit">
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full"
+                                src="/images/profile-9.jpg">
+                            <div
+                                class="absolute bottom-0 right-0 w-3 h-3 border-2 border-white rounded-full bg-theme-10">
+                            </div>
+                        </div>
+                        <div class="ml-2 overflow-hidden">
+                            <div class="flex items-center">
+                                <a href="javascript:;" class="mr-5 font-medium truncate">Leonardo DiCaprio</a>
+                                <div class="ml-auto text-xs text-gray-500 whitespace-nowrap">01:10 PM</div>
+                            </div>
+                            <div class="w-full truncate text-gray-600 mt-0.5">Lorem Ipsum is simply dummy text of
+                                the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s
+                                standard dummy text ever since the 1500</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Notifications -->
+        <!-- BEGIN: Account Menu -->
+        <div class="w-8 h-8 intro-x dropdown">
+            <div class="w-8 h-8 overflow-hidden scale-110 rounded-full shadow-lg dropdown-toggle image-fit zoom-in"
+                role="button" aria-expanded="false">
+                <img alt="Icewall Tailwind HTML Admin Template" src="/images/profile-2.jpg">
+            </div>
+            <div class="w-56 dropdown-menu">
+                <div class="text-white dropdown-menu__content box bg-theme-11 dark:bg-dark-6">
+                    <div class="p-4 border-b border-theme-12 dark:border-dark-3">
+                        <div class="font-medium">{{ auth()->user()->name }}</div>
+                        <div class="text-xs text-theme-13 mt-0.5 dark:text-gray-600">{{ auth()->user()->username }}</div>
+                    </div>
+                    <div class="p-2">
+                        <a href="{{ route('profile.show') }}"
+                            class="flex items-center block p-2 transition duration-300 ease-in-out rounded-md hover:bg-theme-1 dark:hover:bg-dark-3">
+                            <i data-feather="user" class="w-4 h-4 mr-2"></i> Profile </a>
+                        <a href=""
+                            class="flex items-center block p-2 transition duration-300 ease-in-out rounded-md hover:bg-theme-1 dark:hover:bg-dark-3">
+                            <i data-feather="edit" class="w-4 h-4 mr-2"></i> Add Account </a>
+                        <a href=""
+                            class="flex items-center block p-2 transition duration-300 ease-in-out rounded-md hover:bg-theme-1 dark:hover:bg-dark-3">
+                            <i data-feather="lock" class="w-4 h-4 mr-2"></i> Reset Password </a>
+                        <a href=""
+                            class="flex items-center block p-2 transition duration-300 ease-in-out rounded-md hover:bg-theme-1 dark:hover:bg-dark-3">
+                            <i data-feather="help-circle" class="w-4 h-4 mr-2"></i> Help </a>
+                    </div>
+                    <div class="p-2 border-t border-theme-12 dark:border-dark-3">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();this.closest('form').submit();"
+                                class="flex items-center block p-2 transition duration-300 ease-in-out rounded-md hover:bg-theme-1 dark:hover:bg-dark-3">
+                                <i data-feather="toggle-right" class="w-4 h-4 mr-2"></i> Logout </a>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Account Menu -->
+    </div>
+</div>
+<!-- END: Top Bar -->
